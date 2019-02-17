8641da0035bf9d1b5748a997c8deff97123b964a
Empereurs6851 <blabla@gmail.com>
2019-02-17 14:49:58 -0500

    utilisation commune de camera server pour analyse et stream
    Les deux cameras diffusent et une seule analyse

--- vision.py
+++ vision.py
@@ -6,19 +6,24 @@ import time
 from networktables import NetworkTables
 from networktables import NetworkTablesInstance
 import atexit 
+import json
+import numpy as np
 
+configFile = "/boot/frc.json"
+
+class CameraConfig: pass
 
 def FonctionSortie(capture, server, camera):
     print(type(capture))
-    capture.release()
+    # capture.release()
     del server 
     del camera
+
 
 
 def main():
     
     NetworkTables.initialize("10.68.51.2")
-    capture = cv2.VideoCapture("/dev/v4l/by-path/platform-3f980000.usb-usb-0:1.1.3:1.0-video-index0")
     
     
     LineDetect = DetectGreenTape()
@@ -26,25 +31,26 @@ def main():
     sd = NetworkTables.getTable("SmartDashboard")
     ntinst = NetworkTablesInstance.getDefault()
     ntinst.startServer()
+    
     inst = CameraServer.getInstance()
-    camera = UsbCamera("Camera1","/dev/v4l/by-path/platform-3f980000.usb-usb-0:1.2:1.0-video-index0" )
-    server = inst.startAutomaticCapture(camera=camera, return_server=True)
-    atexit.register(FonctionSortie, capture, server, camera)
+    camera1 = UsbCamera("Camera1","/dev/v4l/by-path/platform-3f980000.usb-usb-0:1.2:1.0-video-index0" )
+    camera2 = UsbCamera("Camera2","/dev/v4l/by-path/platform-3f980000.usb-usb-0:1.1.3:1.0-video-index0" )
+    camera1.setConnectionStrategy(VideoSource.ConnectionStrategy.kKeepOpen)
+    camera2.setConnectionStrategy(VideoSource.ConnectionStrategy.kKeepOpen)
+    
+    
+    usb1 = inst.startAutomaticCapture(camera=camera1)
+    usb2 = inst.startAutomaticCapture(camera=camera2)
+    usb1.setResolution(320,240)
+    usb2.setResolution(320,240)    
+    cvSink = inst.getVideo(camera=camera2)
+    img = np.zeros(shape=(240,320,3), dtype=np.uint8)
     while True:
+        timestamp, img = cvSink.grabFrame(img)
+        LineDetect.process(img)
+        sd.putNumber('NbLinesDetectedNew',len(LineDetect.filter_lines_output))
+        time.sleep(0.1)
         
-        ret, frame = capture.read()
-        if ret:
-            LineDetect.process(frame)
-            sd.putNumber('NbLinesDEtected',len(LineDetect.filter_lines_output))    
-        #time.sleep(0.1)
-        
-        
-        
-        
-        #print ("done")
-        #cv2.imshow('frame',frame)
-        #if cv2.waitKey(1) & 0xFF == ord('q'):
-        #    break
     #capture.release()
     #cv2.findContours()
 
